32b392b14cfd9f069b8783799c89d57fab0c154d
sato-kazukiyo <310kazukiyo@gmail.com>
2018-07-10 02:24:56 +0900

    re

--- HelloServlet/src/HelloServlet.java
+++ HelloServlet/src/HelloServlet.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,21 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
-
-	public HelloServlet extends HttpServlet{
-
-		public HelloServlet(){
-			super();
-		}
+	public HelloServlet() {
+	super();
 	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-			PrintWriter out = response.getWriter();
-			out.println("<HTML>");
-			out.println("<BODY>");
-			out.println("<H3>Hello Servlet!</H3>");
-			out.println("</BODY>");
-			out.println("</HTML>");
-			}
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+ServletException, IOException {
+	PrintWriter out = response.getWriter();
+	out.println("<HTML>");
+	out.println("<BODY>");
+	out.println("<H3>Hello Servlet!</H3>");
+	out.println("</BODY>");
+	out.println("</HTML>");
+	}
 }
